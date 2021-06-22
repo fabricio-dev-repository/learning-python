88b0d41ec5b02afc25f951b657b890f4f3adf26f
Fabricio <fabricio.lara@msn.com>
2021-06-22 17:47:49 -0300

    Section 3: Python Object and Data Structure Basics

---
+++ variable_assignments.py
@@ -0,0 +1,20 @@
+#Dynamic Typing
+my_dogs = 2
+my_dogs = ["Sammy","Frankie"]
+#Variables
+a = 5
+print(a)
+a = 10
+print(a)
+print(a+a)
+a = a+a
+print(a)
+#Discover variable type
+print(type(a))
+a = 30.1
+print(type(a))
+#Example
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
